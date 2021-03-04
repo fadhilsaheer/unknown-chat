602816d62724a93beaed85e8ac12634967bbce54
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-04 16:34:02 +0530

    cleand a bit 🧺 broke server code 😭

--- server/server.js
+++ server/server.js
@@ -3,54 +3,18 @@ const app = express();
 
 const server = require("http").createServer(app);
 
-const { v4: uuid } = require("uuid")
+const { v4: uuid } = require("uuid"); // for getting random id
 
-const socketIo = require("socket.io");
-const io = socketIo(server);
-
-// helpers
-
-const { getAllRooms, createRoom, joinRoom } = require("./utils/helper");
-
-// socket io connection
+// socket io connections
+const socket = require("socket.io"); // real time data transfer lib
+const io = socket(server);
 
 io.on("connection", (socket) => {
-
-  // send all rooms when user connects
-
-  socket.on("user-connected", ()=>{
-    let rooms = getAllRooms();
-    socket.emit("all-room", rooms);
-  })
-
-  // create room
-  socket.on("create-room", (roomData)=>{
-    let id  = uuid();
-    roomData.id = id;
-
-    createRoom(roomData);
-    socket.emit("room-created", roomData); // send client room data
-
-  })
-
-  // joining room
-  socket.on("join-room", (roomId, userData)=>{
-
-    joinRoom(roomId, userData)
-    .then((room)=>{
-      socket.emit("joined-room", room)
-    })
-    .catch(()=>{
-      socket.emit("failed-join")
-
-    })
-  })
-
-
+  console.log(`user connected ${socket}`);
 });
 
 // listening server on port
 const serverPort = process.env.PORT || 5000;
 server.listen(serverPort, () =>
-  console.log(`Server started at http://localhost:${serverPort}`)
+  console.log(`server started at port ${serverPort}`)
 );
