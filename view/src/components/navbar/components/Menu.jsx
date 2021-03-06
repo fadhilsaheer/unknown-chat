841cdb49d73cc58ff4188be990d0529eeec16287
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-06 17:47:26 +0530

    added menu item (❁´◡`❁)

--- view/src/components/navbar/components/Menu.jsx
+++ view/src/components/navbar/components/Menu.jsx
@@ -1,6 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faTools } from '@fortawesome/free-solid-svg-icons';
+
 const Menu = () => {
     return (
         <div className="navbar-menu">
+            <h2>Menu</h2>
+            <div>
+                <button className="navbar-menu-current"><FontAwesomeIcon className="navbar-menu-logo" icon={faHome} /> <span>Chats</span></button>
+                <button><FontAwesomeIcon className="navbar-menu-logo" icon={faUser} /> <span>Profile</span></button>
+                <button><FontAwesomeIcon className="navbar-menu-logo" icon={faTools} /> <span>Settings</span></button>
+            </div>
         </div>
     );
 }
