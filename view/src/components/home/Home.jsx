d541b6b290da749e7f2d9c8941b2ec63974f1836
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-06 21:01:10 +0530

    fixed second container bug 😎( iam a pro googler)

--- view/src/components/home/Home.jsx
+++ view/src/components/home/Home.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import Navbar from '../navbar/Navbar';
 import AllChats from './allChats';
 import Buttons from './Buttons';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 const Home = ({ userData }) => {
     const location = useHistory();
 
     if (userData.length === 0) {
-        location.push("/");
+        // location.push("/");
         // sample profile data
         userData = {
             name: "john",
@@ -17,10 +21,19 @@ const Home = ({ userData }) => {
         }
     }
 
+    const [openDrawer, setOpenDrawer] = useState(false);
+
+    const toggleNav = () => {
+        setOpenDrawer(true)
+    }
+
     return (
         <div className="home">
-            <Navbar user={userData} />
+            <Navbar user={userData} setOpenDrawer={setOpenDrawer} openDrawer={openDrawer} />
             <div className="home-app">
+                <div className="navbar-icon">
+                    <button onClick={toggleNav} ><FontAwesomeIcon icon={faBars} /></button>
+                </div>
                 <Buttons />
                 <div className="all-chats-container">
                     <AllChats />
--- view/src/components/navbar/Navbar.jsx
+++ view/src/components/navbar/Navbar.jsx
@@ -1,27 +1,16 @@
-import { useState } from 'react';
-
 import Logo from './components/Logo';
 import Menu from './components/Menu';
 import Profile from './components/Profile';
 import Buttons from './components/Buttons';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import { Drawer } from '@material-ui/core';
 
-const Navbar = ({ user }) => {
-    const [openDrawer, setOpenDrawer] = useState(false);
+const Navbar = ({ user, openDrawer, setOpenDrawer }) => {
 
-    const toggleNav = () => {
-        setOpenDrawer(true)
-    }
 
     return (
         <div className="navbar-container">
-            <div className="navbar-icon">
-                <button onClick={toggleNav} ><FontAwesomeIcon icon={faBars} /></button>
-            </div>
             <div className="navbar navbar-temp">
                 <Logo />
                 <Menu />
