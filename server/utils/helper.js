0b56cc847aa5579266cc9cc5c344e0d7cc7c37d9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-06 19:06:31 +0530

    updated helper (┬┬﹏┬┬)

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -1,38 +1,20 @@
 
 
-const rooms = []; // this will contain all datas of this entire app
-
-/*
-
-// ! user format
-userFormat = {
-    name: "userName",
-    email: "userEmail",
-    profile: "userProfile"
-}
-
-// ! room format
-
-room = [
-    {
-        name: "roomName",
-        id: "roomId",
-        description: "room description",
-        isPublic: true,
-        host: userFormat,
-        users = [
-            userFormat
-        ],
-        chats = [
-            {
-                message: "message",
-                user: userFormat
-            }
-        ]
+const rooms = {
+    "public": {
+        name: "public",
+        id: "public",
+        description: "public server",
+        host: {
+            name: "Public",
+            email: "Public@uchat",
+            profile: "https://www.w3schools.com/w3images/streetart2.jpg",
+        },
+        users: [],
+        chats: [],
     }
-]
+}; // this will contain all datas of this entire app
 
-*/
 
 // room functions
 
@@ -40,26 +22,27 @@ module.exports = {
 
     // rooms
 
-    getAllRooms: ()=>{
+    getAllRooms: () => {
         return rooms;
     },
 
-    getOneRoom: (roomId)=>{
-        return new Promise((resolve, reject)=>{
-            for(room in rooms){
-                if(roomId == room.id){
-                    resolve(room);
-                    break;
-                }
+    getOneRoom: (roomId) => {
+        return new Promise((resolve, reject) => {
+            let room = rooms[roomId];
+
+            if(room != undefined){
+                resolve(room);
+            }else{
+                reject();
             }
-            reject();
+
         })
     },
 
-    // user with room
+    // user 
 
-    joinRoom: (roomId, userData)=>{
-        return new Promise((resolve, reject)=>{
+    joinRoom: (roomId, userData) => {
+        return new Promise((resolve, reject) => {
             this.getOneRoom(roomId).then((room)=>{
                 room.users.push(userData);
                 resolve(room);
@@ -67,39 +50,8 @@ module.exports = {
                 reject();
             })
         })
-
-    },
-
-    createRoom: (roomData)=>{
-        rooms.push(roomData);
-    },
-
-    deleteRoom: (roomId, userData)=>{
-        for(let index=0; index != rooms.length; index++){
-            if(rooms[index].id == roomId){
-                let room = rooms[index];
-                rooms.splice(index, 1);
-                return room;
-            }
-        }
-        return null;
-    },
-
-    // message 
-
-
-    clearChat: (roomId, userData)=>{
-        this.getOneRoom(roomId).then((room)=>{
-            if(room.host == userData){
-                room.chats = [];
-            }
-        })
-    },
-
-    newMessage: (roomId, data)=>{
-        this.getOneRoom(roomId).then((room)=>{
-            room.chats.push(data);
-        })
     }
+
+    // chats
 
 }
