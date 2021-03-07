8cf94461b20cf09f11d93b3a1b57b335391f3b60
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-07 10:04:46 +0530

    working on chat form 📝

--- view/src/components/chat/ChatForm.jsx
+++ view/src/components/chat/ChatForm.jsx
@@ -1,9 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage, faPlane } from '@fortawesome/free-solid-svg-icons';
+
 const ChatForm = () => {
     return (
-        <div className="chat-from">
+        <div className="chat-form">
             <input type="text" placeholder="Enter Message" />
+            <div>
+                <button><FontAwesomeIcon icon={faImage} /></button>
+                <button><FontAwesomeIcon icon={faPlane} /></button>
+            </div>
         </div>
     );
 }
- 
+
 export default ChatForm;
