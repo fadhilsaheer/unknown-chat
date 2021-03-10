975b51e1e6a8d73cd2adc2487286c611b24ff75f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-10 19:50:29 +0530

    updated server for user join 🥳

--- server/server.js
+++ server/server.js
@@ -8,17 +8,26 @@ const server = require("http").createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
 
-const { userJoin } = require("./utils");
+
+let bot = {
+  name: "UBot",
+  profile: 'https://www.w3schools.com/w3images/streetart2.jpg'
+}
 
 io.on("connection", (socket) => {
   // this will take care of chat
 
-  socket.on("join-room", (clientData) => {
-    let serverResult = userJoin(clientData.user, clientData.roomId);
-    if(serverResult != false){
-      socket.emit("join-room", serverResult);
-    };
+  socket.on("join-room", ({ roomId, user, roomName }) => {
+
+    socket.emit("message", { user: bot, message: `Welcome ${user.name} to ${roomName} chat server 😊🎉`, type: "text" });
+    socket.broadcast.to(roomId).emit("message", { user: bot, message: `${user.name} has joined the gang 🥳` });
+
+    socket.join(roomId);
+
+    io.to(roomId).emit("fetchRoomData", roomId);
+
   });
+
 });
 
 const serverPort = process.env.PORT || 8080;
