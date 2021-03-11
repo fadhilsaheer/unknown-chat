e8106071c5dcb13effbc5edbf87d2154e05a4b61
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-11 17:31:06 +0530

    handle socket disconnect 😁

---
+++ server/constants.js
@@ -0,0 +1,5 @@
+const database = 'http://localhost:8000';
+
+module.exports = {
+    database
+}
--- server/server.js
+++ server/server.js
@@ -1,55 +1,76 @@
 const express = require("express");
 const fetch = require("node-fetch");
 
-const Bluebird = require('bluebird');
-fetch.Promise = Bluebird;
+// const Bluebird = require('bluebird');
+// fetch.Promise = Bluebird;
 
 const { v4: uuid } = require("uuid");
+const { database } = require("./constants");
 
 const app = express();
 
 const server = require("http").createServer(app);
 
 const socket = require("socket.io");
+const { resolve, reject } = require("bluebird");
 const io = socket(server);
-
 
 let bot = {
   name: "UBot",
-  profile: 'https://www.w3schools.com/w3images/streetart2.jpg'
-}
+  profile: "https://www.w3schools.com/w3images/streetart2.jpg",
+};
 
+function deleteUser(id) {
+  // fetch user using socket id
+  // user will have a default database id
+  // delete using that database id
+
+  return new Promise((resolve, reject) => {
+    fetch(`${database}/users?socketId=${id}`).then((databaseData) => {
+      databaseData.json().then((user) => {
+        fetch(`${database}/users/${user[0].id}`, {
+          method: "DELETE",
+        });
+
+        resolve(user[0]); // to send all mates about user
+      });
+    });
+  });
+}
 
 io.on("connection", (socket) => {
   // this will take care of chat
 
   socket.on("join-room", ({ roomId, user, roomName }, callback) => {
-
-    socket.emit("message", { user: bot, message: `Welcome ${user.name} to ${roomName} chat server 😊🎉`, type: "text", sender: 'user' });
-    socket.broadcast.to(roomId).emit("message", { user: bot, message: `${user.name} has joined the gang 🥳`, type: "text", sender: 'user' });
+    socket.emit("message", {
+      user: bot,
+      message: `Welcome ${user.name} to ${roomName} chat server 😊🎉`,
+      type: "text",
+      sender: "user",
+    });
+    socket.broadcast
+      .to(roomId)
+      .emit("message", {
+        user: bot,
+        message: `${user.name} has joined the gang 🥳`,
+        type: "text",
+        sender: "user",
+      });
 
     socket.join(roomId);
 
     io.to(roomId).emit("fetchRoomData", roomId);
 
-    callback(socket.id)
-
+    callback(socket.id);
   });
 
   socket.on("disconnect", () => {
-    console.log("disconnected");
-    
-    let id = socket.id
-    fetch(`http://localhost:8000/user?socketId=${id}`, {
-      method: 'DELETE',
-    }).then(()=>{
-      console.log("deleted");
-    })
-    
-    // io.to(roomId).emit("message", { user: bot, message: `someone has left from gang 😥`, type: "text", sender: 'user' });
+    let id = socket.id;
 
+    deleteUser(id).then((user) => {
+      io.to(user.room).emit('message', { user: bot, message: `${user.name} has left the gang 😥`, type: 'text', sender: 'user' });
+    });
   });
-
 });
 
 const serverPort = process.env.PORT || 8080;
