d3392ede9657e584c541d69ebef532d507be84da
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-11 19:40:21 +0530

    added copy feature 😊

--- view/src/components/chat/Buttons.jsx
+++ view/src/components/chat/Buttons.jsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLink, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import { useHistory } from 'react-router-dom';
+import swal from 'sweetalert';
 
 const Buttons = ({ socket }) => {
     const Location = useHistory();
@@ -12,10 +13,15 @@ const Buttons = ({ socket }) => {
         Location.push("/app")
     }
 
+    const copyLink = () => {
+        navigator.clipboard.writeText(window.location);
+        swal("Copied !!", "link copied successfully", "success")
+    }
+
     return (
         <div className="chat-top-buttons">
             <button onClick={handleDisconnect}><FontAwesomeIcon icon={faTimes} /></button>
-            <button><FontAwesomeIcon icon={faLink} /></button>
+            <button onClick={copyLink}><FontAwesomeIcon icon={faLink} /></button>
         </div>
     );
 }
