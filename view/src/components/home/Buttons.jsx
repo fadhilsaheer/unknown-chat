b5db4d85aeb19e5d19a481f5d2ac46801e07c7b5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-12 08:34:19 +0530

    created toggle componetns 😋

--- view/src/components/home/Buttons.jsx
+++ view/src/components/home/Buttons.jsx
@@ -1,17 +1,23 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faPlus, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 import { useHistory } from 'react-router-dom';
 import joinRoom from '../room/joinRoom';
 
 const Buttons = ({ setCurrentMainComponent, components }) => {
     const location = useHistory();
+    const [showClose, setShowClose] = useState(false);
+
+    const handleCreate = () => {
+        setShowClose(!showClose);
+        setCurrentMainComponent(showClose ? components.home : components.create);
+    }
 
     return (
         <div className="top-buttons">
-            <button onClick={() => setCurrentMainComponent(components.create)}><FontAwesomeIcon icon={faPlus} /></button>
+            <button onClick={handleCreate}><FontAwesomeIcon icon={showClose ? faTimes : faPlus} /></button>
             <button onClick={()=> joinRoom(location)}><FontAwesomeIcon icon={faUserPlus} /></button>
         </div>
     );
